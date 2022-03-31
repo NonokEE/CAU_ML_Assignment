2006bd6e552aa1fe077d826681ae87a491ab572d
nonokEE <shshrdl@naver.com>
2022-04-01 00:19:16 +0900

    assignment03_1: retry

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib.colors as colors
+
+I0 = img.imread('03/test.jpeg')
+
+plt.imshow(I0)
+plt.show()
