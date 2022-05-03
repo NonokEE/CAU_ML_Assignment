01a9cd4dafb10107402b49e165be280e6d53aaff
NonokEE <shshrdl@naver.com>
2022-05-04 07:16:32 +0900

    assignment07: compute prediction

--- test.py
+++ test.py
@@ -4,15 +4,6 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import matplotlib.colors as colors
 
-I = np.array([[1, 2, 3],
-              [10, 20, 30],
-              [100, 200, 300]])
-
-
-a = np.zeros([3,5])
-
-for i in range(3):
-    for j in range(5):
-        a[i][j] = i+1
-
+a = np.array([1,2,3,4])
+a = a + 10
 print(a)
