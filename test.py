4e4f407ff7605a16e7de02ae99aaf8403c7e46db
NonokEE <shshrdl@naver.com>
2022-05-12 20:57:16 +0900

    assignment08: function08...?

--- test.py
+++ test.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import matplotlib.colors as colors
 
-a = np.array([1,2,3])
-b = np.array([1,0,1])
-print(a*b)
-print(-a)
+a = np.array([[1,2,3,4],
+              [5,6,7,8],
+              [9,10,11,12]])
+
+
+
+plt.figure()
+im = plt.imshow(a, cmap=cm.jet, norm=colors.LogNorm())
+plt.colorbar(im)
+plt.show()
